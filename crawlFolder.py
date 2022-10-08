ec135fe145dfdea9f6dad8c66791bb57f41079b7
Haydeni0 <hayden.dorahy@gmail.com>
2022-10-08 15:16:05 +0100

    Add argparser for command line arguments

--- crawlFolder.py
+++ crawlFolder.py
@@ -6,6 +6,7 @@ from anytree import Node, RenderTree
 import sys
 from enum import Enum
 from typing import Tuple
+import argparse
 
 
 class Status(Enum):
@@ -14,19 +15,21 @@ class Status(Enum):
     MAX_TIME_EXCEEDED = -1
 
 
-def crawlFolder(root_folder: str, max_depth: int = 3, max_time: float = 1e9) -> Tuple[Node, Status]:
+def crawlFolder(
+    root_folder: str, max_depth: int = 3, max_time: float = 1e9
+) -> Tuple[Node, Status]:
     """
     Python serial os.walk based folder crawler
-    
+
     Conducts a recursive depth first search (up to a maximum depth) of folder.
 
     Args:
         root_folder (str): Path to the folder to use as the root of the search
         max_depth (int, optional): Maximum folder depth to search to. Defaults to 3.
-        max_time (float, optional): Maximum time to spend searching. Defaults to 1e9.
+        max_time (float, optional): Maximum time in seconds to spend searching. Defaults to 1e9.
 
     Returns:
-        Tuple[anytree.Node, Status]: A tuple containing the tree representing the folder 
+        Tuple[anytree.Node, Status]: A tuple containing the tree representing the folder
             structure, and the exit status of the search.
     """
 
@@ -38,7 +41,7 @@ def crawlFolder(root_folder: str, max_depth: int = 3, max_time: float = 1e9) -> 
         status = getChildren(dir_tree, max_depth, end_time)
     else:
         status = Status.MAX_DEPTH_REACHED
-    
+
     duration = time() - start_time
 
     # My default stdout is encoded in cp1252, ensure it changes to utf-8 for special characters
@@ -48,19 +51,18 @@ def crawlFolder(root_folder: str, max_depth: int = 3, max_time: float = 1e9) -> 
     # print(RenderTree(root)) # Don't use this one, as it doesn't look as nice
     for pre, fill, node in RenderTree(dir_tree):
         print(f"{pre}{node.name}")
-    
+
     print(status)
     if status == Status.MAX_TIME_EXCEEDED:
         print(f"Search stopped after reaching the time limit")
     if status == Status.MAX_DEPTH_REACHED:
         print(f"Search completed down to a tree depth of {max_depth}")
 
-
     # Size of a graph is the number of edges (tree order would be the size + 1)
     print(f"Tree size (# of edges): {len(dir_tree.descendants)}")
 
     print(f"Duration: {duration: .2f} seconds")
-        
+
     return dir_tree, status
 
 
@@ -75,7 +77,7 @@ def getChildren(node: Node, max_depth: int, end_time: float) -> Status:
 
     status = Status.OK
     for child_dir in dirnames:
-        c = Node(child_dir, parent = node)
+        c = Node(child_dir, parent=node)
         if c.depth >= max_depth:
             status = Status.MAX_DEPTH_REACHED
         elif time() > end_time:
@@ -87,14 +89,41 @@ def getChildren(node: Node, max_depth: int, end_time: float) -> Status:
                 status = temp_status
 
     for child_file in filenames:
-        Node(child_file, parent = node)
-    
+        Node(child_file, parent=node)
+
     return status
-    
 
 
 if __name__ == "__main__":
-    root_folder = "./test_dir"
-    root_folder = "/mnt/c/Users/Hayden/"
-    dir_tree = crawlFolder(root_folder, max_depth=3, max_time=1)
+
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument(
+        "--root_folder",
+        "-f",
+        dest="root_folder",
+        type=str,
+        default=os.path.expanduser("~"),
+        help="Path to the folder to use as the root of the search. Defaults to the user home directory.",
+    )
+    parser.add_argument(
+        "--max_depth",
+        "-d",
+        dest="max_depth",
+        type=int,
+        default=3,
+        help="Maximum folder depth to search to. Defaults to 3.",
+    )
+    parser.add_argument(
+        "--max_time",
+        "-t",
+        dest="max_time",
+        type=int,
+        default=1e9,
+        help="Maximum time in seconds to spend searching. Defaults to 1e9.",
+    )
+
+    args = parser.parse_args()
+
+    dir_tree = crawlFolder(**vars(args))
+
     pass
