36d1f7ce568a571a84e7ecf3e8127bc47e903689
Haydeni0 <hayden.dorahy@gmail.com>
2022-10-08 15:41:46 +0100

    Add option for output, and relocate print functions

--- crawlFolder.py
+++ crawlFolder.py
@@ -16,7 +16,7 @@ class Status(Enum):
 
 
 def crawlFolder(
-    root_folder: str, max_depth: int = 3, max_time: float = 1e9
+    root_folder: str, max_depth: int = 3, max_time: float = 1e9, print_tree: bool = False
 ) -> Tuple[Node, Status]:
     """
     Python serial os.walk based folder crawler
@@ -33,7 +33,6 @@ def crawlFolder(
             structure, and the exit status of the search.
     """
 
-    start_time = time()
     end_time = time() + max_time
 
     dir_tree = Node(root_folder)
@@ -42,26 +41,14 @@ def crawlFolder(
     else:
         status = Status.MAX_DEPTH_REACHED
 
-    duration = time() - start_time
-
     # My default stdout is encoded in cp1252, ensure it changes to utf-8 for special characters
     sys.stdout.reconfigure(encoding="utf-8")
 
-    # Output the tree in console
-    # print(RenderTree(root)) # Don't use this one, as it doesn't look as nice
-    for pre, fill, node in RenderTree(dir_tree):
-        print(f"{pre}{node.name}")
-
-    print(status)
-    if status == Status.MAX_TIME_EXCEEDED:
-        print(f"Search stopped after reaching the time limit")
-    if status == Status.MAX_DEPTH_REACHED:
-        print(f"Search completed down to a tree depth of {max_depth}")
-
-    # Size of a graph is the number of edges (tree order would be the size + 1)
-    print(f"Tree size (# of edges): {len(dir_tree.descendants)}")
-
-    print(f"Duration: {duration: .2f} seconds")
+    if print_tree:
+        # Output the tree in console
+        # print(RenderTree(root)) # Don't use this one, as it doesn't look as nice
+        for pre, fill, node in RenderTree(dir_tree):
+            print(f"{pre}{node.name}")
 
     return dir_tree, status
 
@@ -96,6 +83,7 @@ def getChildren(node: Node, max_depth: int, end_time: float) -> Status:
 
 if __name__ == "__main__":
 
+    # Parse command line arguments
     parser = argparse.ArgumentParser(description="")
     parser.add_argument(
         "--root_folder",
@@ -121,9 +109,32 @@ if __name__ == "__main__":
         default=1e9,
         help="Maximum time in seconds to spend searching. Defaults to 1e9.",
     )
+    parser.add_argument(
+        "--print_tree",
+        "-p",
+        action='store_true', # "store_true" makes the default false
+        help="Set true to print the tree. Defaults to False.",
+    )
 
     args = parser.parse_args()
+    args = vars(args)
 
-    dir_tree = crawlFolder(**vars(args))
+    # Run the folder crawler
+    start_time = time()
+    dir_tree, status = crawlFolder(**args)
+    duration = time() - start_time
+
+    # Print status message
+    if status == Status.MAX_TIME_EXCEEDED:
+        print(f"Search stopped after reaching the time limit.")
+    if status == Status.MAX_DEPTH_REACHED:
+        md = args["max_depth"]
+        print(f"Search completed down to a tree depth of {md}.")
+    if status == Status.OK:
+        print(f"Search completed.")
+
+    # Size of a graph is the number of edges (tree order would be the size + 1)
+    print(f"Tree size (# of edges): {len(dir_tree.descendants)}")
+    print(f"Duration: {duration: .2f} seconds")
 
     pass
